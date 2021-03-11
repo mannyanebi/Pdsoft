f01313461105fbf13a9861805c00802e8e166092
Anebi Emmanuel <anebiemmanuel@gmail.com>
2021-03-11 12:36:44 +0100

    Added a test POST request test case for the contactus route

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+# from django.test import TestCase
+import json
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import ContactDetail
+from .serializers import ContactDetailSerializer
 
 # Create your tests here.
+class ContactDetailTestCase(APITestCase):
+    
+    def test_contactdetail(self):
+        data = {"fullname": "Tester 01", "email": "tester@pdsoft.app", 
+        "message": "I am an automated test"}
+
+        response = self.client.post("/contactus/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
